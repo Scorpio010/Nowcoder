8fc8ea62516ac7406de87a8227b78b7bca64187f
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-01-01 23:47:55 +0800

    Add files via upload

---
+++ 牛客eb49750ef0de47168c21761de086d97c.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class 牛客eb49750ef0de47168c21761de086d97c {
+    public static void main(String[] args) {
+        Scanner sca = new Scanner(System.in);
+        int score1 = sca.nextInt();
+        int score2 = sca.nextInt();
+        int score3 = sca.nextInt();
+        System.out.println("score1="+score1+",score2="+score2+",score3="+score3);
+    }
+}
