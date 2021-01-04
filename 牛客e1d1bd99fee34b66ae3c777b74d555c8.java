2c88c67e5390c4d2eab24c089f9726f00f174a08
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-01-04 17:20:51 +0800

    Add files via upload

---
+++ 牛客e1d1bd99fee34b66ae3c777b74d555c8.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class 牛客e1d1bd99fee34b66ae3c777b74d555c8 {
+    public static void main(String[] args) {
+        Scanner sca = new Scanner(System.in);
+        int age = sca.nextInt();
+        long second = (long)age*31560000;
+        System.out.println(second);
+    }
+}
+
