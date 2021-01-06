e1a42468acdefe8e549c003d8830d5679d172496
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-01-06 14:25:20 +0800

    Add files via upload

---
+++ 牛客0f5d9bfcd63b47fda2052a583b1fbd1f.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class 牛客0f5d9bfcd63b47fda2052a583b1fbd1f {
+    public static void main(String[] args) {
+        Scanner sca = new Scanner(System.in);
+        double r = sca.nextDouble();
+        double V = (double)4/3*3.1415926*Math.pow(r,3);
+        System.out.printf("%.3f",V);
+    }
+}
