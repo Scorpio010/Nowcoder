00131878d4099bc1b1ee21e9ab89c3a9852ef53d
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-01-08 23:54:30 +0800

    Add files via upload

---
+++ 牛客8e94458049eb4e838f711bbd1be0045e.java
@@ -0,0 +1,7 @@
+public class 牛客8e94458049eb4e838f711bbd1be0045e {
+    public static void main(String[] args) {
+        System.out.println("Name    Age    Gender");
+        System.out.println("---------------------");
+        System.out.println("Jack    18     man");
+    }
+}
