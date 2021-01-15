99dd61383a819785dcb5042617d9835fa0a06fcb
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-01-15 23:53:58 +0800

    Add files via upload

---
+++ 牛客4ffcc9f206b949ddb057ee0099b34d51.java
@@ -0,0 +1,6 @@
+public class 牛客4ffcc9f206b949ddb057ee0099b34d51 {
+    public static void main(String[] args) {
+        System.out.printf("0"+"%o",1234);
+        System.out.printf(" 0X"+"%x",1234);
+    }
+}
