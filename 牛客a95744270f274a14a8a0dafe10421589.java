da9908688252d1e85110a48ce2c24bd9787b67c3
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-02-04 22:34:46 +0800

    Add files via upload

---
+++ 牛客a95744270f274a14a8a0dafe10421589.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class 牛客a95744270f274a14a8a0dafe10421589 {
+    public static void main(String[] args){
+        Scanner sca = new Scanner(System.in);
+        while(sca.hasNext()){
+            int n = sca.nextInt();
+            for (int i=0;i<n;i++){
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
