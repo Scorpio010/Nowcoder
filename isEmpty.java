18cb1924c170c93caa8c00280fe2c5bcebfdad0c
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-02-23 22:49:51 +0800

    Add files via upload

---
+++ isEmpty.java
@@ -0,0 +1,7 @@
+public class isEmpty {
+    public static void main(String[] args) {
+        String s ="";
+        System.out.println(s.isEmpty());
+        System.out.println(s.length() == 0);
+    }
+}
